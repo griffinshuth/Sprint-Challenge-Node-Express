0cdbc3192f16f76fd80135ec6f949e9847375a90
Griffin <griffinshuth@gmail.com>
2018-12-08 10:59:33 -0700

    finished mvp

--- Review.md
+++ Review.md
@@ -2,16 +2,32 @@
 
 ## What is Node.js?
 
+Its a runtime enviorment that executes JS apps outside the browser.
+
 ## What is Express?
+
+a web app that sits on node.js it allows routing and middleware. 
 
 ## Mention two parts of Express that you learned about this week.
 
+middleware and routing
+
 ## What is Middleware?
+
+its a function that gets the req and res and operates on them. 
 
 ## What is a Resource?
 
+Anything returned by a Restful API. 
+
 ## What can the API return to help clients know if a request was successful?
+
+status code 200
 
 ## How can we partition our application into sub-applications?
 
+routes
+
 ## What is express.json() and why do we need it?
+
+It is built in middlewar and we need it to parse incoming requests with json payloads.
--- index.js
+++ index.js
@@ -1,7 +1,152 @@
 const express = require('express');
 
-const servver = express();
+const actionModel = require('./data/helpers/actionModel');
+const projectModel = require('./data/helpers/projectModel');
+
+const server = express();
 
 const PORT = 4000;
 
-servver.get()
+
+server.get('/action', (req, res) => {
+    actionModel.get()
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        res.status(500).json({error: "cant get id" })
+    });
+    });
+
+
+server.get('/project', (req, res) => {
+    projectModel.get()
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        res.status(500).json({error: "cant get id" })
+    });
+    });
+
+    server.get('/action/:id', (req, res) => {
+        const id = req.params.id;
+        actionModel.get(id)
+              .then (post => {
+                res.status(200).json(post)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't recieve the action with id."})
+              });
+    
+    });
+    
+    
+    server.post ('/action', (req, res) => {
+        const {description, notes, project_id} = req.body;
+        actionModel.insert({description, notes, project_id})
+              .then (insert => {
+                res.status(200).json(insert)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't post it!"})
+              });
+    });
+    
+    
+    server.put('/action/:id',(req, res) => {
+        const id = req.params.id;
+        const post = req.body;
+        actionModel.update(id, post)
+              .then (update => {
+                res.json(update)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't be modified."})
+              });
+    
+    });
+    
+    
+    server.delete('/action/:id', (req, res) => {
+        const id = req.params.id;
+    
+        actionModel.remove(id)
+              .then (remove => {
+                res.status(200).json(remove)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't remove the action."})
+              });
+    
+    });
+    
+    server.get('/project/:id', (req, res) => {
+        const id = req.params.id;
+        projectModel.get(id)
+              .then (post => {
+                res.status(200).json(post)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't recieve the project with id."})
+              });
+    
+    });
+    
+    server.post ('/project', (req, res) => {
+        const {name, description} = req.body;
+        projectModel.insert({name, description})
+              .then (post => {
+                res.status(200).json(post)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't post the project."})
+              });
+    
+    
+    });
+    
+    
+    server.put('/project/:id', (req, res) => {
+        const id = req.params.id;
+        const changes = req.body;
+        projectModel.update(id, changes)
+              .then (update => {
+                res.json(update)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't be modified."})
+              });
+    
+    });
+    
+    
+    server.delete('/project/:id', (req, res) => {
+        const id = req.params.id;
+    
+        projectModel.remove(id)
+              .then (remove => {
+                res.status(200).json(remove)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't remove the project."})
+              });
+    
+    });
+    
+    server.get('/project/action/:id', (req, res) => {
+        const id = req.params.id;
+        projectModel.getProjectActions(id)
+              .then (post => {
+                res.status(200).json(post)
+              })
+              .catch(err => {
+                  res.status(500).json({error: "Can't recieve the project with id."})
+              })
+    
+    });
+    
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
